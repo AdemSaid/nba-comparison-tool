e75eb18541da9afd99153876e8644674195ecb1c
AdemSaid <adem_said@hotmail.com>
2024-09-18 20:46:45 -0400

    Rename Player.tsx to PlayerCard.tsx

---
+++ src/app/components/PlayerCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Player } from '../data/players';
+
+interface PlayerCardProps {
+  player: Player;
+  isSelected: boolean;
+  onSelect: (player: Player) => void;
+}
+
+export const PlayerCard: React.FC<PlayerCardProps> = ({ player, isSelected, onSelect }) => (
+  <div 
+    className={`p-2 rounded cursor-pointer transition-colors ${
+      isSelected ? 'bg-blue-200 hover:bg-blue-300' : 'bg-gray-100 hover:bg-gray-200'
+    }`}
+    onClick={() => onSelect(player)}
+  >
+    <span className="font-bold">{player.name}</span> - {player.team}
+  </div>
+);
